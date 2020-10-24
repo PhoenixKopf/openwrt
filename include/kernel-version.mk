039e0e05829620fc5f5ea05287a000726378b9d3
Felix Fietkau <nbd@nbd.name>
2020-10-24 21:14:16 +0200

    kernel: add linux 5.10 support
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,8 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .86
+LINUX_VERSION-5.10 = .4
 
 LINUX_KERNEL_HASH-5.4.86 = eb36b5fc6ef7b953acba0a3e62d872e0330c4d34b38d58f5714493a4fe3b0e8b
+LINUX_KERNEL_HASH-5.10.4 = 904e396c26e9992a16cd1cc989460171536bed7739bf36049f6eb020ee5d56ec
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
